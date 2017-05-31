067d2f6d463ef7f250b7dafd832e512143db6834
Peter Kang <peter.kang@cmlmediasoft.local>
2017-05-31 14:54:46 -0700

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="wrap">
+        <div className="bg">
+        </div>
+        <div className="image-wrap">
+          <img className="top-image" src="https://s3-us-west-2.amazonaws.com/kendrickdamnproject/kendrick+lamar+damn/welcome-top.png" alt="#" />
+        <a href="http://www.kendricklamar.com/" alt="#"><h1>damn.</h1></a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
