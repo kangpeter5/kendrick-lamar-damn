09fa0df49de4b7d0b640ec7d54f230d8377948c7
Peter Kang <peter.kang@cmlmediasoft.local>
2017-06-08 09:01:03 -0700

    implementing bootstrap on home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,14 +11,10 @@ const NavbarInstance = (
       <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-
-      </Nav>
       <Nav pullRight>
         <NavItem eventKey={1} href="/home">/home</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-        <NavDropdown eventKey={3} title="Albums" id="basic-nav-dropdown">
+        <NavItem eventKey={2} href="#">about</NavItem>
+        <NavDropdown eventKey={3} title="albums" id="basic-nav-dropdown">
 			<MenuItem eventKey={3.1}>Section.80</MenuItem>
 			<MenuItem eventKey={3.2}>Good Kid, M.A.A.D. City</MenuItem>
 			<MenuItem eventKey={3.3}>To Pimp a Butterfly</MenuItem>
@@ -31,7 +27,6 @@ const NavbarInstance = (
 );
 
 class Header extends Component {
-
   render() {
     return (
 		NavbarInstance
