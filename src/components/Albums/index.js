53e8c15f2efad0d4e75bf2bee59318fd053de2c3
Peter Kang <peter.kang@cmlmediasoft.local>
2017-06-19 09:48:05 -0700

    fixed padding issues on body and container

--- src/components/Albums/index.js
+++ src/components/Albums/index.js
@@ -27,8 +27,10 @@ class Albums extends Component {
 					<div className="album-desc">
 						<h3>Good Kid, M.A.A.D City</h3>
 						<p>The second studio album by KDot debuted at number two on the Billboard 200 chart, selling 242,000 copies in its first week – earning the highest first-week hip hop album sales of 2012 from a male artist, along with the best-selling debut from a male artist of the year. It became Lamar's first album to enter the UK Albums Chart, peaking at number 16, and entering the UK R&B Albums Chart at number two. The album was also named to many end-of-the-year lists, often topping them. It was later certified Platinum by the Recording Industry Association of America (RIAA) in August 2013. By March 2015, the album had sold 1,400,000 copies domestically, according to Nielsen SoundScan.</p>
-						<Button href="http://pitchfork.com/reviews/albums/17253-good-kid-maad-city/">Pitchfork</Button>
-						<Button href="http://www.complex.com/music/2012/10/the-making-of-kendrick-lamars-good-kid-maad-city/before-the-album">Complex</Button>
+						<div className="center-btn">
+							<Button href="http://pitchfork.com/reviews/albums/17253-good-kid-maad-city/">Pitchfork</Button>
+							<Button href="http://www.complex.com/music/2012/10/the-making-of-kendrick-lamars-good-kid-maad-city/before-the-album">Complex</Button>
+						</div>
 					</div>
 				</Col>
 				<Col md={6} mdPull={6}>
@@ -41,8 +43,10 @@ class Albums extends Component {
 					<div className="album-desc">
 						<h3>To Pimp a Butterfly</h3>
 						<p>To Pimp a Butterfly is the third studio album by King Kendrick. The album incorporates elements of free jazz, funk, soul, spoken word, and the avant-garde and explores a variety of political and personal themes concerning African-American culture, racial inequality, depression, and institutional discrimination. It debuted atop the Billboard 200 and received widespread acclaim from critics, who praised its musical scope and the social relevance of Lamar's lyrics. It was ranked as the best album of 2015 by many publications, including Rolling Stone, Billboard and Pitchfork. It was nominated for Album of the Year and won Best Rap Album at the 58th Grammy Awards.</p>
-						<Button href="http://pitchfork.com/reviews/albums/20390-to-pimp-a-butterfly/">Pitchfork</Button>
-						<Button href="http://www.xxlmag.com/news/2017/01/kendrick-lamars-to-pimp-a-butterfly-archived-harvard-library/">XXL</Button>
+						<div className="center-btn">
+							<Button href="http://pitchfork.com/reviews/albums/20390-to-pimp-a-butterfly/">Pitchfork</Button>
+							<Button href="http://www.xxlmag.com/news/2017/01/kendrick-lamars-to-pimp-a-butterfly-archived-harvard-library/">XXL</Button>
+						</div>
 					</div>
 				</Col>
 				<Col xs={12} md={6}>
@@ -54,8 +58,10 @@ class Albums extends Component {
 					<div className="album-desc">
 						<h3>Damn</h3>
 						<p>Damn received widespread acclaim from critics. At Metacritic, which assigns a normalized rating out of 100 to reviews from mainstream publications, the album received an average score of 96, based on 37 reviews. Writing for The A.V. Club, Evan Rytlewski concluded, "Lamar trusts every idea to stand on its own. When you're making art this substantial, vital, and virtuosic, there's no need to wrap a tidy bow around it." In his review, Greg Kot of Chicago Tribute states, "Damn. strips down the rhythms to their essence, flavored with the occasional cameo (notably Rihanna and U2). Lamar's voice does most of the heavy lifting, playing multiple roles and characters. His supple singing complements a variety of rap tones and textures."</p>
-						<Button href="http://pitchfork.com/reviews/albums/23147-damn/">Pitchfork</Button>
-						<Button href="http://www.xxlmag.com/rap-music/reviews/2017/04/kendrick-lamar-damn-album-review/">XXL</Button>
+						<div className="center-btn">
+							<Button href="http://pitchfork.com/reviews/albums/23147-damn/">Pitchfork</Button>
+							<Button href="http://www.xxlmag.com/rap-music/reviews/2017/04/kendrick-lamar-damn-album-review/">XXL</Button>
+						</div>
 					</div>
 				</Col>
 				<Col md={6} mdPull={6}>
