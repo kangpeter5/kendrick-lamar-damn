c579c91e7e9289022758a836eea86a846b541f23
Peter Kang <peter.kang@cmlmediasoft.local>
2017-06-21 15:47:23 -0700

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,24 +15,23 @@ class Header extends Component {
 		  <Navbar inverse collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="/home">KDOT</a>
+            <a href="/">KDOT</a>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem eventKey={1} href="/home">/home</NavItem>
-            <NavItem eventKey={2} href="#aboutMe">/about</NavItem>
-            <NavItem eventKey={3} href="https://dribbble.com/shots/3550308-DAMN/attachments/789463" alt="#" target="_blank" id="art-link">/art</NavItem>
+            <NavItem eventKey={1} href="#aboutMe">/about</NavItem>
+            <NavItem eventKey={2} href="https://dribbble.com/shots/3550308-DAMN/attachments/789463" alt="#" target="_blank" id="art-link">/art</NavItem>
             <NavDropdown 
-              eventKey={4}
+              eventKey={3}
               title="/albums"
               id="basic-nav-dropdown"
             >
-              <MenuItem eventKey={4.1} href="#section80">Section.80</MenuItem>
-              <MenuItem eventKey={4.2} href="#madd">Good Kid, M.A.A.D. City</MenuItem>
-              <MenuItem eventKey={4.3} href="#pimp">To Pimp a Butterfly</MenuItem>
-              <MenuItem eventKey={4.3} href="#damn">DAMN.</MenuItem>
+              <MenuItem eventKey={3.1} href="#section80">Section.80</MenuItem>
+              <MenuItem eventKey={3.2} href="#madd">Good Kid, M.A.A.D. City</MenuItem>
+              <MenuItem eventKey={3.3} href="#pimp">To Pimp a Butterfly</MenuItem>
+              <MenuItem eventKey={3.4} href="#damn">DAMN.</MenuItem>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
