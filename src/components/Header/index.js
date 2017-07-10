52c7a6bd7c4e753c26a9dc7f3ea02e6bbe32d4ce
Peter Kang <peter.kang@cmlmediasoft.local>
2017-07-10 14:31:24 -0700

    update navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -20,17 +20,16 @@ class Header extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem eventKey={1} href="#aboutMe">/about</NavItem>
-            <NavItem eventKey={2} href="https://dribbble.com/shots/3550308-DAMN/attachments/789463" alt="#" target="_blank" id="art-link">/art</NavItem>
+            <NavItem eventKey={1} href="https://dribbble.com/shots/3550308-DAMN/attachments/789463" alt="#" target="_blank">/art</NavItem>
             <NavDropdown 
-              eventKey={3}
+              eventKey={2}
               title="/albums"
               id="basic-nav-dropdown"
             >
-              <MenuItem eventKey={3.1} href="#section80">Section.80</MenuItem>
-              <MenuItem eventKey={3.2} href="#madd">Good Kid, M.A.A.D. City</MenuItem>
-              <MenuItem eventKey={3.3} href="#pimp">To Pimp a Butterfly</MenuItem>
-              <MenuItem eventKey={3.4} href="#damn">DAMN.</MenuItem>
+              <MenuItem eventKey={2.1} href="#section80">Section.80</MenuItem>
+              <MenuItem eventKey={2.2} href="#madd">Good Kid, M.A.A.D. City</MenuItem>
+              <MenuItem eventKey={2.3} href="#pimp">To Pimp a Butterfly</MenuItem>
+              <MenuItem eventKey={2.4} href="#damn">DAMN.</MenuItem>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
